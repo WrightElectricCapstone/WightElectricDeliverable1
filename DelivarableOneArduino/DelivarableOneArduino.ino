72d3efb91542e73f7aed79fd87472a9d540a8245
AlexanderFWolff <alexander_wolff@umail.ucsb.edu>
2017-10-31 22:32:04 -0700

    Functional Commit
    Changed way includes are handled
    This commit works

--- DelivarableOneArduino/DelivarableOneArduino.ino
+++ DelivarableOneArduino/DelivarableOneArduino.ino
@@ -1,23 +1,28 @@
-#include <C:\Users\thewo\Documents\DelivarableOneArduino\PololuMaestro.h>
+#include <PololuMaestro.h>
 
 #define maestroSerial SERIAL_PORT_HARDWARE_OPEN
 MiniMaestro maestro(maestroSerial);
 
 uint8_t ChannelEntered = 0;
-uint8_t ChannelValue = 0;
+uint16_t ChannelValue = 0;
 bool ChannelStage = true;
-int TransmitStatus = 1;
+bool TransmitStatus = false;
+
 
 int TransmitChannel(uint8_t ChannelNum, uint8_t ChannelVal);
+bool SetTarget(unsigned char Channel, unsigned short Target);
 
 void setup() {
   Serial.begin(9600);
-  SerialUSB.begin(9600);
   maestroSerial.begin(9600);
+  maestro.setSpeed(0, 0);
+      maestro.setAcceleration(0, 0);
+      delay(1000);
   Serial.println("Enter Channel Number:");
 }
 
 void loop() {
+
   if(Serial.available() && ChannelStage)
   {
       ChannelEntered = Serial.parseInt();
@@ -25,6 +30,7 @@ void loop() {
       Serial.print(ChannelEntered);
       Serial.print(":\n");
       ChannelStage = false;
+      delay(200);
   }
   if(Serial.available() && !ChannelStage)
   {
@@ -53,9 +59,21 @@ void loop() {
   }
 }
 
-int TransmitChannel(uint8_t ChannelNum, uint8_t ChannelVal)
+int TransmitChannel(uint8_t ChannelNum, uint16_t ChannelVal)
 {
-  maestro.setTargetMiniSSC(ChannelNum, ChannelVal);
+  maestro.setTarget(ChannelNum, ChannelVal);
   return 0; 
+}
+
+
+bool SetTarget(unsigned char Channel, unsigned short Target)
+{
+  unsigned char Command[4];
+  Command[0] = 0x84;
+  Command[1] = Channel;
+  Command[2] = Target & 0x7F;
+  Command[3] = (Target >> 7) & 0x7F;
+  SerialUSB.write(Command, 4);
+  return true;
 }
 
